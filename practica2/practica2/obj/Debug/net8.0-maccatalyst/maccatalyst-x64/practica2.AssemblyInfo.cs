2167169e39956b14437c191c7a0f447d055dccce
Jose-Pater294 <jose.paterninas@cecar.edu.co>
2024-10-17 21:11:47 -0500

    añadi un boton

--- practica2/practica2/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/practica2.AssemblyInfo.cs
+++ practica2/practica2/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/practica2.AssemblyInfo.cs
@@ -14,11 +14,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("practica2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+a254dd9cff3c18ca34d9397cf69887889ac659c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("practica2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("practica2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
-[assembly: System.Runtime.Versioning.TargetPlatformAttribute("MacCatalyst17.5")]
+[assembly: System.Runtime.Versioning.TargetPlatformAttribute("MacCatalyst17.2")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("MacCatalyst13.1")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
